88ed76f5ed4ada0f1e826985c14332bf80b024e2
Shubham Kumar <kumarshubham96341@gmail.com>
2024-02-22 19:42:44 +0530

    hello

---
+++ gitpush.sh
@@ -0,0 +1,14 @@
+git status
+
+git add .
+
+echo 'Enter commit message'
+read commitMessage
+
+git commit -m "$commitMessage"
+
+echo 'Enter name of branch'
+read branch
+
+git push origin $branch
+read
