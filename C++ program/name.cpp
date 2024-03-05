1b033db3529f1b5e63ebd254a507ccc36d827ef3
Shubham Kumar <kumarshubham96341@gmail.com>
2024-03-05 20:13:15 +0530

    Name

---
+++ C++ program/name.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,n;
+
+    cout<<"Enter the value of n:"<<endl;
+    cin>>n;
+
+    cout<<"My name is:"<<endl;
+    for(i=0;i<n;i++)
+    {
+        cout<<"Shubham Kumar"<<endl;
+    }
+    cout<<"Thank You..."<<endl;
+}
+
+
+/*
+----------output----------
+Enter the value of n:
+5
+My name is:
+Shubham Kumar
+Shubham Kumar
+Shubham Kumar
+Shubham Kumar
+Shubham Kumar
+Thank You...
+*/
