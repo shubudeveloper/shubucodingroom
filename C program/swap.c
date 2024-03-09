942038fcf5793afcc73a0abefb4f485c14585cd8
Shubham Kumar <kumarshubham96341@gmail.com>
2024-03-09 12:59:44 +0530

    Swap using two variables

---
+++ C program/swap.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+
+    printf("Enter the value of a:");
+    scanf("%d",&a);
+
+    printf("Enter the value of b:");
+    scanf("%d",&b);
+
+    printf("Before swapping:\n");
+    printf("a=%d and b=%d\n1",a,b);
+
+    a=a+b;                               //swapping using two variables
+    b=a-b;
+    a=a-b;
+
+    printf("After swapping:\n");
+    printf("a=%d and b=%d\n",a,b);
+
+    printf("Swapping is done successfully...");
+
+}
+
+
+/*
+----------output----------
+Enter the value of a:30
+Enter the value of b:20
+Before swapping:
+a=30 and b=20
+1After swapping:
+a=20 and b=30
+Swapping is done successfully...
+*/
