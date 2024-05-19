d49dc6520b6e40fa4dd263c495650cb0b185038c
Shubham Kumar <kumarshubham96341@gmail.com>
2024-05-19 16:26:59 +0530

    Project

---
+++ Python/first.py
@@ -0,0 +1,11 @@
+print("This is my first program in python")
+print("Shubham Kumar")
+
+
+'''
+--------------------output--------------------
+
+This is my first program in python
+Shubham Kumar
+
+'''
